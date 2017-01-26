385d8b62cc5de958e885e612c767396242bd0995
Josh Thompson <thompsonjoshd@gmail.com>
2017-01-25 17:10:57 -0700

    finish a working version of BubbleSort

--- sorting/bubble_sort.rb
+++ sorting/bubble_sort.rb
@@ -1,22 +1,24 @@
-# pseudocode
-# http://backend.turing.io/module1/projects/sorting_suite
-
-# [4, 2]
+require 'pry'
 
 class BubbleSort
-  def sort(collection)
-    # temp_array = []
-    collection.each_with_index do |value, index|
-      if value > collection[index + 1]
-        puts "i'm bigger"
+  def sort(input)
+    (input.length - 1).times do
+      input.map.with_index do |value, index|
+        if input[(index + 1)] == nil
+        elsif input[index] > input[(index + 1)]
+          input[index], input[(index + 1)] = input[(index + 1)], input[index]
+        end
       end
     end
+    p input
   end
 end
 
+e = BubbleSort.new
+# collection = ([6, 3, 8, 9, 1, 4, 0, 5, 2, 7])
+collection = [9,8,7,7,6,5,4,3,2,1,0]
+e.sort(collection)
 
-sorter = BubbleSort.new
-sorter.sort([4, 2])
 # sorter.sort([4, 3, 8, 6, 2, 5, 9, 7, 1, 0])
 # sorter.sort(['d', 'b', 'a', 'c'])
 # => ["a", "b", "c", "d"]
