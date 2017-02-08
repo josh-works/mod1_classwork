11b4a7940892eb946fb16a088664d94a4a7ae236
Josh Thompson <thompsonjoshd@gmail.com>
2017-02-08 10:35:56 -0700

    enums, hash shit

--- hash_depth/hash_bonanza.rb
+++ hash_depth/hash_bonanza.rb
@@ -2,26 +2,11 @@ require 'pry'
 # Write a method that takes a hash and returns the depth of the deepest value.
 
 class HashDepth
-
-# hash = { :key_1 => { :key_2 => { :key_3_a => { :key_4 => "found me!" }, :key_3_b => "not quite" } } }
-
-  def find_depth(hash, count = 0)
-    count = nil
-    hash.each do |value|
-      count += 1 if value.class == Hash
-      find_depth(value, count)
+  def find_depth(hash)
+    if hash.values.any? { |value| value.instance_of?(Hash) }
+      2
+    else
+      1
     end
-    # check = hash.values.any? do |value|
-    #   value.class == Hash
-    # end
-    # if check
-    #   find_depth(hash)
-    # else
-    #   1
-    # end
-
   end
-
-
-# => 4
 end
--- hash_depth/hash_bonanza_test.rb
+++ hash_depth/hash_bonanza_test.rb
@@ -3,33 +3,47 @@ require 'minitest/autorun'
 require_relative 'hash_bonanza.rb'
 
 class HashBonanzaTest < Minitest::Test
-  # def test_can_find_one_level_of_depth
-  #   hash = { :key => "value" }
-  #   hd = HashDepth.new
-  #   assert_equal 1, hd.find_depth(hash)
-  # end
-  #
-  # def test_can_find_two_levels_depth
-  #   hd = HashDepth.new
-  #   hash = { :key => { :key1 => "value" } }
-  #   assert_equal 2, hd.find_depth(hash)
-  # end
-  #
-  # def test_can_find_two_levels_depth_with_2_keys
-  #   hd = HashDepth.new
-  #   hash = { :key => "value",
-  #         :key2 => { :key1 => "value" } }
-  #   assert_equal 2, hd.find_depth(hash)
-  # end
-  #
-  # def test_can_find_three_levels_depth_with_2_keys
-  #   hd = HashDepth.new
-  #   hash = { :key => "value",
-  #         :key2 => { :key1 => "value" } }
-  #   assert_equal 2, hd.find_depth(hash)
-  # end
+  def test_can_find_one_level_of_depth
+    hash = { :key => "value" }
+    hd = HashDepth.new
+    assert_equal 1, hd.find_depth(hash)
+  end
+
+  def test_can_find_two_levels_depth
+    skip
+    hd = HashDepth.new
+    hash = { :key => { :key1 => "value" } }
+    assert_equal 2, hd.find_depth(hash)
+  end
+
+  def test_can_find_depth_of_three
+    hd = HashDepth.new
+    hash = {
+      :key1a => { :key2a => "value" },
+      :key1b => "value",
+      :key1c => { :key2b => { :key3a => "value" }}
+      }
+      assert_equal 3, hd.find_depth(hash)
+  end
+
+  def test_can_find_two_levels_depth_with_2_keys
+    skip
+    hd = HashDepth.new
+    hash = { :key => "value",
+          :key2 => { :key1 => "value" } }
+    assert_equal 2, hd.find_depth(hash)
+  end
+
+  def test_can_find_three_levels_depth_with_2_keys
+    skip
+    hd = HashDepth.new
+    hash = { :key => "value",
+          :key2 => { :key1 => "value" } }
+    assert_equal 2, hd.find_depth(hash)
+  end
 
   def test_one_level_depth_has_count_0
-    
+    skip
+
   end
 end
